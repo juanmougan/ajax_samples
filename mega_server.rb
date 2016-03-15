73189e817f6736366e8a2d5510ff9e6b773a9e1e
Juan Manuel Mougan <jmougan@hexacta.com>
2016-03-15 14:55:05 -0300

    Works from Postman, but not from the browser

--- mega_server.rb
+++ mega_server.rb
@@ -15,12 +15,26 @@ get '/' do
 end
 
 get '/pedidos' do
-   { :person_name => "#{params[:person_name]}" }.to_json
+   received_params = { :person_name => "#{params[:person_name]}" }.to_json
+   puts received_params
+   return received_params
 end
 
 post '/pedidos/guardar' do
   #{}"I got #{params[:person_name]}."
   #@pedidos << params[:person_name]
-  { :person_name => "#{params[:person_name]}" }.to_json
-  #@pedidos
+  #puts "Person name #{params[:person_name]}"
+  #puts "USing JSON: #{JSON.parse params[:person_name]}"
+
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+  puts "The payload is: #{request_payload}"
+  puts "First name: #{request_payload["person_name"]}"
+  puts "Amount: #{request_payload["amount"]}"
+  puts "Discount: #{request_payload["discount"]}"
+
+  received_params = { :person_name => "#{request_payload["person_name"]}", 
+  						:amount => "#{request_payload["amount"]}",
+  						:discount => "#{request_payload["discount"]}" }.to_json
+  return received_params
 end
